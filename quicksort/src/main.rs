a06921825e505d34578f5133d29a9f7c0fc52c60
devgioele <gioeledevittidev@gmail.com>
2022-04-17 16:51:49 +0200

    Update binary crate with new quicksort

--- quicksort/src/main.rs
+++ quicksort/src/main.rs
@@ -1,17 +1,17 @@
 mod lib;
 
-use quicksort::sort::{quicksort, quicksort_concurrent};
+use quicksort::sort::{quicksort, quicksort_seq};
 
 fn main() {
     let mut vec1: Vec<f64> = vec![1.0, 9.7, 3.4, 4.0, -3.14];
     let mut vec2 = vec1.clone();
     let vec_sorted = [-3.14, 1.0, 3.4, 4.0, 9.7];
 
-    // Serial
-    quicksort(&mut vec1);
+    // Sequential
+    quicksort_seq(&mut vec1);
     assert_eq!(vec1, vec_sorted);
 
-    // Concurrent
-    quicksort_concurrent(&mut vec2);
+    // Potentially concurrent
+    quicksort(&mut vec2);
     assert_eq!(vec2, vec_sorted);
 }
