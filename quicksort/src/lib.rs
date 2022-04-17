2d00ce9adca2d77e26263209e26ff2abe5b37520
devgioele <gioeledevittidev@gmail.com>
2022-04-17 12:05:04 +0200

    Progress towards a functioning concurrent quicksort

--- quicksort/src/lib.rs
+++ quicksort/src/lib.rs
@@ -1,5 +1,11 @@
+extern crate threadpool;
+
 pub mod sort {
-    /// Computes the media for a given vector of numbers.
+    use std::sync::mpsc::channel;
+    use std::sync::Mutex;
+    use threadpool::ThreadPool;
+
+    /// Computes the media for a given arrtor of numbers.
     /// The media is the value separating the higher half from
     /// the lower half of a data sample.
     ///
@@ -8,39 +14,39 @@ pub mod sort {
     /// A data set with an odd number of values:
     /// ```
     /// use quicksort::sort::median;
-    /// let vec = vec![1.0,3.0,3.0,6.0,7.0,8.0,9.0];
-    /// let median = median(&vec[..]);
+    /// let arr = [1.0,3.0,3.0,6.0,7.0,8.0,9.0];
+    /// let median = median(&arr);
     /// assert_eq!(median, 6.0);
     /// ```
     ///
     /// A data set with an even number of values:
     /// ```
     /// use quicksort::sort::median;
-    /// let vec = vec![1.0,2.0,3.0,4.0,5.0,6.0,8.0,9.0];
-    /// let median = median(&vec[..]);
+    /// let arr = [1.0,2.0,3.0,4.0,5.0,6.0,8.0,9.0];
+    /// let median = median(&arr);
     /// assert_eq!(median, 4.5);
     /// ```
-    pub fn median(vec: &[f64]) -> f64 {
-        let len = vec.len();
+    pub fn median(arr: &[f64]) -> f64 {
+        let len = arr.len();
         return if len % 2 == 0 {
-            (vec[len / 2 - 1] + vec[len / 2]) / 2f64
+            (arr[len / 2 - 1] + arr[len / 2]) / 2f64
         } else {
-            vec[(len + 1) / 2 - 1]
+            arr[(len + 1) / 2 - 1]
         };
     }
 
-    fn swap(vec: &mut [f64], a: usize, b: usize) {
-        let old_a = vec[a];
-        vec[a] = vec[b];
-        vec[b] = old_a;
+    fn swap(arr: &mut [f64], a: usize, b: usize) {
+        let old_a = arr[a];
+        arr[a] = arr[b];
+        arr[b] = old_a;
     }
 
     /// Unstable partitioning algorithm.
     ///
     /// returns: index of the pivot, such that the left partition is <= the pivot and
     /// the right partition is > the pivot.
-    fn partition_hoare(vec: &mut [f64], low: usize, high: usize) -> usize {
-        let pivot = vec[(high + low) / 2];
+    fn partition_hoare(arr: &mut [f64], low: usize, high: usize) -> usize {
+        let pivot = arr[(high + low) / 2];
         // Set indices taking possible overflows into account
         let (mut left, mut skip_left) = if low == 0 {
             (low, true)
@@ -61,7 +67,7 @@ pub mod sort {
                 } else {
                     left += 1;
                 }
-                if vec[left] >= pivot {
+                if arr[left] >= pivot {
                     break;
                 }
             }
@@ -72,35 +78,69 @@ pub mod sort {
                 } else {
                     right -= 1;
                 }
-                if vec[right] <= pivot {
+                if arr[right] <= pivot {
                     break;
                 }
             }
+            // If indices crossed
             if left >= right {
                 break;
             }
-            swap(vec, left, right);
+            swap(arr, left, right);
         }
         right
     }
 
-    fn quicksort_rec(vec: &mut [f64], low: usize, high: usize) {
+    fn quicksort_rec(arr: &mut [f64], low: usize, high: usize) {
         // Base case
         if low >= high {
             return;
         }
         // Continue by induction
-        let pivot = partition_hoare(vec, low, high);
-        quicksort_rec(vec, low, pivot);
-        quicksort_rec(vec, pivot + 1, high);
+        let pivot = partition_hoare(arr, low, high);
+        quicksort_rec(arr, low, pivot);
+        quicksort_rec(arr, pivot + 1, high);
     }
 
-    /// Unstable sorting of the given vector slice.
-    pub fn quicksort(vec: &mut [f64]) {
-        let len = vec.len();
-        if len > 0 {
-            quicksort_rec(vec, 0, len - 1);
+    /// Unstable sorting of the given array.
+    pub fn quicksort(arr: &mut [f64]) {
+        quicksort_rec(arr, 0, arr.len() - 1);
+    }
+
+    fn quicksort_concurrent_rec(
+        pool: &'static Mutex<ThreadPool>,
+        arr: &'static mut [f64],
+        low: usize,
+        high: usize,
+    ) {
+        // Base case
+        if low >= high {
+            return;
         }
+        // Continue by induction
+        let pivot = partition_hoare(arr, low, high);
+        let (tx, rx) = channel();
+        {
+            let tx = tx.clone();
+            pool.lock().unwrap().execute(move || {
+                quicksort_concurrent_rec(pool, arr, low, pivot);
+                tx.send(1);
+            });
+        }
+        {
+            let tx = tx.clone();
+            pool.lock().unwrap().execute(move || {
+                quicksort_concurrent_rec(pool, arr, pivot + 1, high);
+                tx.send(1);
+            });
+        }
+        // Wait for the threads to finish
+        rx.iter().take(2).collect::<Vec<i32>>();
+    }
+
+    pub fn quicksort_concurrent(pool: &'static Mutex<ThreadPool>, arr: &'static mut [f64]) {
+        quicksort_concurrent_rec(pool, arr, 0, arr.len() - 1);
+        pool.lock().unwrap().join();
     }
 
     #[cfg(test)]
@@ -110,210 +150,125 @@ pub mod sort {
         mod partition {
             use super::*;
 
-            fn partition_hoare_whole(vec: &mut [f64]) -> usize {
-                partition_hoare(vec, 0, vec.len() - 1)
+            fn partition_hoare_whole(arr: &mut [f64]) -> usize {
+                partition_hoare(arr, 0, arr.len() - 1)
             }
 
             #[test]
             fn hoare_one() {
-                let mut vec = vec![-3.3];
-                let pivot = partition_hoare_whole(&mut vec);
+                let mut arr = [-3.3];
+                let pivot = partition_hoare_whole(&mut arr);
                 assert_eq!(pivot, 0);
-                assert_eq!(vec, [-3.3]);
+                assert_eq!(arr, [-3.3]);
             }
 
             #[test]
             fn hoare_two_unsorted() {
-                let mut vec = vec![9.0, 8.0];
-                let pivot = partition_hoare_whole(&mut vec);
+                let mut arr = [9.0, 8.0];
+                let pivot = partition_hoare_whole(&mut arr);
                 assert_eq!(pivot, 0);
-                assert_eq!(vec, [8.0, 9.0]);
+                assert_eq!(arr, [8.0, 9.0]);
             }
 
             #[test]
             fn hoare_even_sorted() {
-                let mut vec = vec![1.0, 2.4, 3.0, 7.0];
-                let pivot = partition_hoare_whole(&mut vec);
+                let mut arr = [1.0, 2.4, 3.0, 7.0];
+                let pivot = partition_hoare_whole(&mut arr);
                 assert_eq!(pivot, 1);
-                assert_eq!(vec, [1.0, 2.4, 3.0, 7.0]);
+                assert_eq!(arr, [1.0, 2.4, 3.0, 7.0]);
             }
 
             #[test]
             fn hoare_even_sorted_slice() {
-                let mut vec = vec![1.0, 2.4, 3.0, 7.0, 16.4, 902.1, -703.2, 9.2];
-                let pivot = partition_hoare(&mut vec, 1, 4);
+                let mut arr = [1.0, 2.4, 3.0, 7.0, 16.4, 902.1, -703.2, 9.2];
+                let pivot = partition_hoare(&mut arr, 1, 4);
                 assert_eq!(pivot, 2);
-                assert_eq!(vec, [1.0, 2.4, 3.0, 7.0, 16.4, 902.1, -703.2, 9.2]);
+                assert_eq!(arr, [1.0, 2.4, 3.0, 7.0, 16.4, 902.1, -703.2, 9.2]);
             }
 
             #[test]
             fn hoare_equal() {
-                let mut vec = vec![3.0, 3.0, 3.0];
-                let pivot = partition_hoare_whole(&mut vec);
+                let mut arr = [3.0, 3.0, 3.0];
+                let pivot = partition_hoare_whole(&mut arr);
                 assert_eq!(pivot, 1);
-                assert_eq!(vec, [3.0, 3.0, 3.0]);
+                assert_eq!(arr, [3.0, 3.0, 3.0]);
             }
 
             #[test]
             fn hoare_odd_sorted() {
-                let mut vec = vec![2.3, 3.0, 4.0];
-                let pivot = partition_hoare_whole(&mut vec);
+                let mut arr = [2.3, 3.0, 4.0];
+                let pivot = partition_hoare_whole(&mut arr);
                 assert_eq!(pivot, 1);
-                assert_eq!(vec, [2.3, 3.0, 4.0]);
+                assert_eq!(arr, [2.3, 3.0, 4.0]);
             }
 
             #[test]
             fn hoare_even_unsorted() {
-                let mut vec = vec![1.0, 7.1, 2.2, 8.0];
-                let pivot = partition_hoare_whole(&mut vec);
+                let mut arr = [1.0, 7.1, 2.2, 8.0];
+                let pivot = partition_hoare_whole(&mut arr);
                 assert_eq!(pivot, 1);
-                assert_eq!(vec, [1.0, 2.2, 7.1, 8.0]);
+                assert_eq!(arr, [1.0, 2.2, 7.1, 8.0]);
             }
 
             #[test]
             fn hoare_odd_unsorted() {
-                let mut vec = vec![9.2, 3.1, 4.0];
-                let pivot = partition_hoare_whole(&mut vec);
+                let mut arr = [9.2, 3.1, 4.0];
+                let pivot = partition_hoare_whole(&mut arr);
                 assert_eq!(pivot, 0);
-                assert_eq!(vec, [3.1, 9.2, 4.0]);
+                assert_eq!(arr, [3.1, 9.2, 4.0]);
             }
+        }
+
+        #[test]
+        fn quicksort_one() {
+            let mut arr = [9.7];
+            quicksort(&mut arr);
+            assert_eq!(arr, [9.7]);
+        }
+
+        #[test]
+        fn quicksort_two_unsorted() {
+            let mut arr = [3.4, 1.0];
+            quicksort(&mut arr);
+            assert_eq!(arr, [1.0, 3.4]);
         }
 
         #[test]
         fn quicksort_even_sorted() {
-            let mut vec: Vec<f64> = vec![1.0, 9.7, 3.4, 4.0];
-            quicksort(&mut vec);
-            assert_eq!(vec, [1.0, 3.4, 4.0, 9.7]);
+            let mut arr = [1.0, 9.7, 3.4, 4.0];
+            quicksort(&mut arr);
+            assert_eq!(arr, [1.0, 3.4, 4.0, 9.7]);
         }
 
         #[test]
         fn quicksort_odd_sorted() {
-            let mut vec: Vec<f64> = vec![1.0, 9.7, 3.4, 4.0, -3.14];
-            quicksort(&mut vec);
-            assert_eq!(vec, [-3.14, 1.0, 3.4, 4.0, 9.7]);
+            let mut arr = [1.0, 9.7, 3.4, 4.0, -3.14];
+            quicksort(&mut arr);
+            assert_eq!(arr, [-3.14, 1.0, 3.4, 4.0, 9.7]);
         }
 
         #[test]
-        fn quicksort_even_sorted_concurrent() {
-            let vec: Vec<f64> = vec![1.0, 9.7, 3.4, 4.0];
-
-            // TODO:
-            // 1. Start the sorting
-            // 2. Wait for the sorting to complete
+        fn quicksort_concurrent_serial() {
+            let mut arr1 = [1.0, 9.7, 3.4, 4.0];
+            let mut arr2 = arr1.clone();
+            let pool = Mutex::new(ThreadPool::new(2));
+            assert_eq!(quicksort(&mut arr1), quicksort_concurrent(&pool, &mut arr2));
         }
 
         #[test]
-        fn quicksort_odd_sorted_concurrent() {
-            let vec: Vec<f64> = vec![1.0, 9.7, 3.4, 4.0, -3.14];
-
-            // TODO:
-            // 1. Start the sorting
-            // 2. Wait for the sorting to complete
-        }
-    }
-}
-
-pub mod threads {
-
-    use std::sync::mpsc;
-    use std::sync::mpsc::{Receiver, Sender};
-    use std::sync::{Arc, Mutex};
-    use std::thread;
-    use std::thread::{sleep, JoinHandle};
-    use std::time::Duration;
-
-    pub enum ThreadState {
-        IDLE,
-        WORKING,
-    }
-
-    pub struct PoolState {
-        thread_states: Mutex<Vec<ThreadState>>,
-    }
-
-    pub struct Pool<T> {
-        txs: Vec<Sender<T>>,
-        pub threads: Vec<JoinHandle<()>>,
-        state: Arc<PoolState>,
-    }
-
-    type ThreadClosure = dyn Fn() + Send + 'static;
-
-    impl Pool<Box<ThreadClosure>> {
-        pub fn new(size: usize) -> Self {
-            let mut txs = vec![];
-            let mut threads = vec![];
-            let mut thread_states = vec![];
-            for _ in 0..size {
-                thread_states.push(ThreadState::IDLE);
-            }
-
-            let state = Arc::new(PoolState {
-                thread_states: Mutex::from(thread_states),
-            });
-
-            for id in 0..size {
-                let (tx, rx) = mpsc::channel();
-
-                txs.push(tx);
-
-                let state = Arc::clone(&state);
-                threads.push(thread::spawn(move || {
-                    Pool::worker_thread(state, id, rx);
-                }));
-            }
-
-            Pool {
-                txs,
-                threads,
-                state,
-            }
+        fn quicksort_even_unsorted_concurrent() {
+            let mut arr = [1.0, 9.7, 3.4, 4.0];
+            let pool = Mutex::new(ThreadPool::new(2));
+            quicksort_concurrent(&pool, &mut arr);
+            assert_eq!(arr, [1.0, 3.4, 4.0, 9.7]);
         }
 
-        pub fn execute(&self, block: Box<ThreadClosure>) {
-            let thread_id;
-
-            {
-                let thread_states = self.state.thread_states.lock().unwrap();
-                thread_id = thread_states
-                    .iter()
-                    .position(|thread_state| match thread_state {
-                        ThreadState::IDLE => true,
-                        ThreadState::WORKING => false,
-                    })
-                    .or_else(|| Option::Some(0))
-                    .unwrap();
-            }
-
-            println!("Submitting task to thread {}...", thread_id);
-
-            // TODO: Cover case in which sending fails
-            self.txs[thread_id].send(block);
-
-            // We sleep to give the opportunity the thread to acquire the lock of the thread state. If we don't
-            // wait, we will end up having the lock acquired by all the calls to "execute".
-            sleep(Duration::new(0, 100))
-        }
-
-        fn worker_thread(state: Arc<PoolState>, id: usize, rx: Receiver<Box<ThreadClosure>>) {
-            loop {
-                for closure in rx.iter() {
-                    {
-                        let mut state = state.thread_states.lock().unwrap();
-
-                        (*state)[id] = ThreadState::WORKING;
-                        println!("Thread {} working...", id);
-                    }
-
-                    closure();
-
-                    {
-                        let mut state = state.thread_states.lock().unwrap();
-                        (*state)[id] = ThreadState::IDLE;
-                        println!("Thread {} idle...", id);
-                    }
-                }
-            }
+        #[test]
+        fn quicksort_odd_unsorted_concurrent() {
+            let mut arr = [1.0, 9.7, 3.4, 4.0, -3.14];
+            let pool = Mutex::new(ThreadPool::new(2));
+            quicksort_concurrent(&pool, &mut arr);
+            assert_eq!(arr, [-3.14, 1.0, 3.4, 4.0, 9.7]);
         }
     }
 }
--- quicksort/src/main.rs
+++ quicksort/src/main.rs
@@ -1,8 +1,20 @@
 mod lib;
-use lib::sort::quicksort;
+
+use quicksort::sort::{quicksort, quicksort_concurrent};
+use std::sync::Mutex;
+use threadpool::ThreadPool;
 
 fn main() {
-    let mut vec: Vec<f64> = vec![1.0, 9.7, 3.4, 4.0, -3.14];
-    quicksort(&mut vec);
-    assert_eq!(vec, [-3.14, 1.0, 3.4, 4.0, 9.7]);
+    let mut vec1: Vec<f64> = vec![1.0, 9.7, 3.4, 4.0, -3.14];
+    let mut vec2 = vec1.clone();
+    let vec_sorted = [-3.14, 1.0, 3.4, 4.0, 9.7];
+    let pool = Mutex::new(ThreadPool::new(2));
+
+    // Serial
+    quicksort(&mut vec1);
+    assert_eq!(vec1, vec_sorted);
+
+    // Concurrent
+    quicksort_concurrent(&pool, &mut vec2);
+    assert_eq!(vec2, vec_sorted);
 }
